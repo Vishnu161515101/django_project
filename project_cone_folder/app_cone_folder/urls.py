4202823304ddc0a5d8360ae8db4e9b2df62bb58f
bumannagarivishnu143@gmail.com <bumannagarivishnu143@gmail.com>
2024-04-17 12:47:56 +0530

    second

--- project_cone_folder/app_cone_folder/urls.py
+++ project_cone_folder/app_cone_folder/urls.py
@@ -2,7 +2,9 @@ from django.urls import path
 from  . import views
 
 
+
 urlpatterns = [
-    path('entry_page',views.entry_page,name='entry_page')
+    path('entry_page',views.entry_page,name='entry_page'),
+    path('entry_page1',views.entry_page1,name='entry_page1'),
    
 ]
--- project_cone_folder/app_cone_folder/views.py
+++ project_cone_folder/app_cone_folder/views.py
@@ -1,7 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
 
 # Create your views here.
 
 def entry_page(request):
     return HttpResponse('hello vishnu come back to django')
+
+def entry_page1(request):
+   template = loader.get_template('entry_pages_for_cone.html')
+   return HttpResponse(template.render())
